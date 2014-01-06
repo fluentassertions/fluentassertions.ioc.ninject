37fab795a2d88723694b09987f0a0fa5fbc61a54
Kevin Kuszyk <kevin@kevinkuszyk.com>
2014-01-06 20:48:37 +0000

    Added assembly.Should().Reference() assertions.

--- src/FluentAssertions.Ioc.Ninject/AssemblyAssertions.cs
+++ src/FluentAssertions.Ioc.Ninject/AssemblyAssertions.cs
@@ -46,5 +46,38 @@ namespace FluentAssertions.Ioc.Ninject
                    .ForCondition(!references.Any(x => x == assemblyName))
                    .FailWith("Assembly {0} should not reference assembly {1}{reason}", subjectName, assemblyName);
         }
+
+        /// <summary>
+        /// Asserts that an assembly references the specifed assembly.
+        /// </summary>
+        /// <param name="assembly">The assembly which should be referenced.</param>
+        public void Reference(Assembly assembly)
+        {
+           Reference(assembly, String.Empty); 
+        }
+
+        /// <summary>
+        /// Asserts that an assembly references the specifed assembly.
+        /// </summary>
+        /// <param name="assembly">The assembly which should be referenced.</param>
+        /// <param name="reason">
+        /// A formatted phrase as is supported by <see cref="string.Format(string,object[])" /> explaining why the assertion 
+        /// is needed. If the phrase does not start with the word <i>because</i>, it is prepended automatically.
+        /// </param>
+        /// <param name="reasonArgs">
+        /// Zero or more objects to format using the placeholders in <see cref="reason" />.
+        /// </param>
+        public void Reference(Assembly assembly, string reason, params string[] reasonArgs)
+        {
+            var subjectName = Subject.GetName().Name;
+            var assemblyName = assembly.GetName().Name;
+
+            var references = Subject.GetReferencedAssemblies().Select(x => x.Name);
+
+            Execute.Assertion
+                   .BecauseOf(reason, reasonArgs)
+                   .ForCondition(references.Any(x => x == assemblyName))
+                   .FailWith("Assembly {0} should reference assembly {1}{reason}, but it does not", subjectName, assemblyName);
+        }
     }
 }
--- src/FluentAssertions.Ioc.Samples.Core/SampleService.cs
+++ src/FluentAssertions.Ioc.Samples.Core/SampleService.cs
@@ -1,7 +1,13 @@
 ﻿
+using FluentAssertions.Ioc.Samples.Entities;
+
 namespace FluentAssertions.Ioc.Samples.Core
 {
     public class SampleService
     {
+        public void SomeMethod()
+        {
+            var entity = new Entity();
+        }
     }
 }
