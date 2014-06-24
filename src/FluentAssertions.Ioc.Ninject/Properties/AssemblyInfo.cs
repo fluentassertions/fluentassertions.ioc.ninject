c5c012e900647c3381fd2bf4bf6f0b3535fe2ff2
Kevin Kuszyk <kevin@kevinkuszyk.com>
2014-06-24 11:59:01 +0100

    Set nuget version to 1.0.0-beta1

--- src/FluentAssertions.Ioc.Ninject/Properties/AssemblyInfo.cs
+++ src/FluentAssertions.Ioc.Ninject/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("FluentAssertions.Ioc.Ninject")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Kevin Kuszyk")]
 [assembly: AssemblyProduct("FluentAssertions.Ioc.Ninject")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCopyright("Copyright © Kevin Kuszyk 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -34,3 +34,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0-beta1")]
