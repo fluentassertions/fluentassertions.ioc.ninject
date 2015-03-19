89eeb9661c82dba1327ac2315e312fdcd6ce868b
Kevin Kuszyk <kevin@kevinkuszyk.com>
2015-03-19 14:43:06 +0000

    Update copyright year

--- src/FluentAssertions.Ioc.Ninject/Properties/AssemblyInfo.cs
+++ src/FluentAssertions.Ioc.Ninject/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Kevin Kuszyk")]
 [assembly: AssemblyProduct("FluentAssertions.Ioc.Ninject")]
-[assembly: AssemblyCopyright("Copyright © Kevin Kuszyk 2014")]
+[assembly: AssemblyCopyright("Copyright © Kevin Kuszyk 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
