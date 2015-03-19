46ab5a509aa40ac34459cd20ad4e0149f9293dbe
Kevin Kuszyk <kevin@kevinkuszyk.com>
2015-03-19 14:31:18 +0000

    Moving type extensions to their own class

--- src/FluentAssertions.Ioc.Ninject/AssemblyExtensions.cs
+++ src/FluentAssertions.Ioc.Ninject/AssemblyExtensions.cs
@@ -24,35 +24,5 @@ namespace FluentAssertions.Ioc.Ninject
         {
             return assembly.GetTypes().Where(x => x.IsInterface);
         }
-
-        /// <summary>
-        /// Filters for just the types in the specified namespace.
-        /// </summary>
-        /// <typeparam name="T">The namespace of this type.</typeparam>
-        /// <param name="types">The types to filter.</param>
-        public static IEnumerable<Type> InNamespaceOf<T>(this IEnumerable<Type> types)
-        {
-            return types.Where(x => x.Namespace == typeof(T).Namespace);
-        }
-
-        /// <summary>
-        /// Filters to exclude the specified type.
-        /// </summary>
-        /// <typeparam name="T">The type to exclude.</typeparam>
-        /// <param name="types">The types to filter.</param>
-        public static IEnumerable<Type> Excluding<T>(this IEnumerable<Type> types)
-        {
-            return types.Where(x => x.Name != typeof(T).Name);
-        }
-
-        /// <summary>
-        /// Filters to only include types where the type name ends with the specified string.
-        /// </summary>
-        /// <param name="types">The types to filter.</param>
-        /// <param name="endingWith">The ending with string.</param>
-        public static IEnumerable<Type> EndingWith(this IEnumerable<Type> types, string endingWith)
-        {
-            return types.Where(x => x.Name.EndsWith(endingWith));
-        }
     }
 }
---
+++ src/FluentAssertions.Ioc.Ninject/TypeExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FluentAssertions.Ioc.Ninject
+{
+    public static class TypeExtensions
+    {
+        /// <summary>
+        /// Filters for just the types in the specified namespace.
+        /// </summary>
+        /// <typeparam name="T">The namespace of this type.</typeparam>
+        /// <param name="types">The types to filter.</param>
+        public static IEnumerable<Type> InNamespaceOf<T>(this IEnumerable<Type> types)
+        {
+            return types.Where(x => x.Namespace == typeof(T).Namespace);
+        }
+
+        /// <summary>
+        /// Filters to exclude the specified type.
+        /// </summary>
+        /// <typeparam name="T">The type to exclude.</typeparam>
+        /// <param name="types">The types to filter.</param>
+        public static IEnumerable<Type> Excluding<T>(this IEnumerable<Type> types)
+        {
+            return types.Where(x => x.Name != typeof(T).Name);
+        }
+
+        /// <summary>
+        /// Filters to only include types where the type name ends with the specified string.
+        /// </summary>
+        /// <param name="types">The types to filter.</param>
+        /// <param name="endingWith">The ending with string.</param>
+        public static IEnumerable<Type> EndingWith(this IEnumerable<Type> types, string endingWith)
+        {
+            return types.Where(x => x.Name.EndsWith(endingWith));
+        }
+    }
+}
